b49cc05912b674caf02a7e5536479354b02c78a8
Ludi <ludisantos3004@gmail.com>
2024-11-01 15:16:06 -0400

    tela exame,inseminacao, apartacao, paricao, transporte arrumei ordenha e funcionario

---
+++ RuralTech/Telas/TelaInseminacao.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RuralTech.Telas
+{
+    /// <summary>
+    /// Lógica interna para TelaInseminacao.xaml
+    /// </summary>
+    public partial class TelaInseminacao : Window
+    {
+        public TelaInseminacao()
+        {
+            InitializeComponent();
+        }
+
+        private void AutoSuggestBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
